800dca3b25a247abe467fde04dd178b886eb8dce
layton <layton.ab10@gmail.com>
2026-07-25 13:52:27 -0400

    spyder overlay

--- overlays/sypder.nix
+++ overlays/sypder.nix
@@ -22,10 +22,8 @@ final: prev: {
     nativeBuildInputs = [ prev.makeWrapper ];
     
     postBuild = ''
-      # 2. Delete all symlinks in /bin to hide `python`, `black`, etc. from your terminal
       rm -rf $out/bin/*
       
-      # 3. Create a new wrapper specifically for Spyder, injecting your Qt fixes
       makeWrapper ${spyderEnv}/bin/spyder $out/bin/spyder \
         --set QT_QPA_PLATFORM xcb \
         --set QT_XCB_GL_INTEGRATION none \
